8f876b21b6e515d3e229ed9c0d4b3aeccd772f53
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2022-11-13 19:01:00 -0500

    Pagina busqueda

--- controller/web.py
+++ controller/web.py
@@ -2,5 +2,19 @@ from flask import render_template
 from application import app
 
 @app.route("/")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
+
+@app.route("/busqueda.html")
+def busqueda():
+    return render_template("busqueda.html")
+
+@app.route("/buscar")
+def buscar():
+    return render_template("busqueda.html")
+
+@app.route("/detalle/<id>")
+def detalle(id):
+    return render_template("detalle.html")
+
